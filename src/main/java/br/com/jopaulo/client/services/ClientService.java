7fa20fc8d2ba027173a093a73ff354aa16e091c2
João Paulo <jp_cbc@hotmail.com>
2021-05-25 11:53:02 -0300

    Resources, services, busca paginada

---
+++ src/main/java/br/com/jopaulo/client/services/ClientService.java
@@ -0,0 +1,24 @@
+package br.com.jopaulo.client.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.jopaulo.client.dto.ClientDTO;
+import br.com.jopaulo.client.entities.Client;
+import br.com.jopaulo.client.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+}
