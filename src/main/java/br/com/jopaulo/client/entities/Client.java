f314b421edc4516bdc579dfcf6d31d098baa7784
João Paulo <jp_cbc@hotmail.com>
2021-05-25 10:45:45 -0300

    DTO

--- src/main/java/br/com/jopaulo/client/entities/Client.java
+++ src/main/java/br/com/jopaulo/client/entities/Client.java
@@ -1,5 +1,6 @@
 package br.com.jopaulo.client.entities;
 
+import java.io.Serializable;
 import java.time.Instant;
 
 import javax.persistence.Column;
@@ -11,8 +12,10 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_client")
-public class Client {
-
+public class Client implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
